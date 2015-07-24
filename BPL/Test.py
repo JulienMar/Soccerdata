196b90753235a19776581722c99e99a6e6a1b77d
Julien <julien.marbaix@hotmail.com>
2015-07-24 12:41:25 +0200

    Managed to get team names

--- BPL/Test.py
+++ BPL/Test.py
@@ -1,10 +1,24 @@
 from lxml import html
 import requests
+from bs4 import BeautifulSoup
+import re
 
 page = requests.get('http://www.bbc.com/sport/football/premier-league/results')
 tree = html.fromstring(page.text)
 
 links = tree.xpath('//a[@class="report"]/@href')
 
+string = 'http://www.bbc.com/sport/0/football/' + links[0][-8:]
 
-print(len(links))
+matchPage = requests.get(string)
+
+data = matchPage.text
+
+soup = BeautifulSoup(data, "lxml")
+
+teams = soup.find_all("h3", class_="team-name")
+clean_team = []
+for x in teams:
+    clean_team.append(re.sub('<[^<]+?>', '', str(x)))
+
+print(clean_team)
